2459aa32abbb3e36bb8c0ecf93ec3a384b89ceee
Ting-Jun Wang <levi900227@gmail.com>
2021-05-15 15:07:09 +0800

    feat: 新增area controller(show, store)

--- backend/app/Http/Controllers/PlaceAreaController.php
+++ backend/app/Http/Controllers/PlaceAreaController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PlaceArea;
 use Illuminate\Http\Request;
 
 class PlaceAreaController extends Controller
 {
-    //
+    public function show($id)
+    {
+        return PlaceArea::findOrFail($id);
+    }
+
+    public function store(Request $request)
+    {
+        $area = new PlaceArea();
+        $area->name = $request->input('name');
+
+        $area->save();
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MealController;
+use App\Http\Controllers\PlaceAreaController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,14 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::prefix('user')->group(function(){
     Route::post('/login', [UserController::class, 'login']);
     Route::post('/', [UserController::class, 'register']);
 });
+Route::prefix('place_area')->group(function(){
+    Route::get('/{id}', [PlaceAreaController::class, 'show']);
+});
+
 
 Route::middleware('auth:sanctum')->group(function(){
-    Route::get('/user', [UserController::class, 'show']);
-    Route::delete('/user', [UserController::class, 'delete']);
+    Route::prefix('/user')->group(function(){
+        Route::get('/', [UserController::class, 'show']);
+        Route::delete('/', [UserController::class, 'delete']);
+    });
+
+    Route::prefix('place_area')->group(function(){
+        Route::post('/', [PlaceAreaController::class, 'store']);
+    });
 });
