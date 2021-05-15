0124b86ff34deb8318879896827808ef247b8c66
linwebs <thomas881114@gmail.com>
2021-05-15 16:44:22 +0800

    編輯 shop api

--- backend/app/Http/Controllers/ShopController.php
+++ backend/app/Http/Controllers/ShopController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
+	public function index()
+	{
+		return Shop::all();
+	}
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,22 +19,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('user')->group(function(){
+Route::prefix('/user')->group(function(){
     Route::post('/login', [UserController::class, 'login']);
     Route::post('/', [UserController::class, 'register']);
 });
-Route::prefix('place_area')->group(function(){
+Route::prefix('/place_area')->group(function(){
     Route::get('/', [PlaceAreaController::class, 'index']);
     Route::get('/{id}', [PlaceAreaController::class, 'show']);
     Route::get('/{id}/builds', [PlaceAreaController::class, 'getBuilds']);
 });
-Route::prefix('place_build')->group(function(){
+Route::prefix('/place_build')->group(function(){
     Route::get('/{id}', [PlaceBuildController::class, 'show']);
     Route::get('/{id}/rooms', [PlaceBuildController::class, 'getRooms']);
 });
 
-Route::prefix('place_room')->group(function(){
-    Route::get('/{id}', [PlaceRoomController::class, 'show']);
+Route::prefix('/place_room')->group(function(){
+	Route::get('/{id}', [PlaceRoomController::class, 'show']);
+});
+
+Route::prefix('/shop')->group(function(){
+	Route::get('/', [PlaceAreaController::class, 'index']);
 });
 
 Route::middleware('auth:sanctum')->group(function(){
@@ -43,15 +47,15 @@ Route::middleware('auth:sanctum')->group(function(){
         Route::delete('/', [UserController::class, 'delete']);
     });
 
-    Route::prefix('place_area')->group(function(){
+    Route::prefix('/place_area')->group(function(){
         Route::post('/', [PlaceAreaController::class, 'store']);
     });
 
-    Route::prefix('place_build')->group(function(){
+    Route::prefix('/place_build')->group(function(){
         Route::post('/', [PlaceBuildController::class, 'store']);
     });
 
-    Route::prefix('place_room')->group(function(){
+    Route::prefix('/place_room')->group(function(){
         Route::post('/', [PlaceRoomController::class, 'store']);
     });
 
