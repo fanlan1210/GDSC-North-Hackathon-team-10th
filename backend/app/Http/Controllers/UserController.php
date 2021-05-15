cb0e9678a385473405de6ea206dda9546ee357f9
Ting-Jun Wang <levi900227@gmail.com>
2021-05-15 14:38:09 +0800

    feat: User相關

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use PhpOption\None;
+
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('account', $request->input('account'))->first();
+
+        if ($user){
+            if($user->password == hash('sha256', $request->input('password'))){
+                $token = $user->createToken('api_token')->plainTextToken;
+                return response($token);
+            }
+        }
+
+        return response("fail", 401);
+    }
+
+    public function register(Request $request)
+    {
+        $user = new User;
+        $user->account = $request->input('account');
+        $user->name = $request->input('name');
+        $user->phone = $request->input('phone');
+        $user->password = hash('sha256', $request->input('password'));
+        $user->email = $request->input('email');
+        $user->type = 0;
+
+        $user->save();
+
+        return response('ok');
+    }
+
+    public function show(Request $request)
+    {
+        return $request->user();
+    }
+
+    public function delete(Request $request)
+    {
+        $user = User::find($request->user()->id);
+        $user->delete();
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::prefix('user')->group(function(){
+    Route::post('/login', [UserController::class, 'login']);
+    Route::post('/', [UserController::class, 'register']);
+});
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/user', [UserController::class, 'show']);
+    Route::delete('/user', [UserController::class, 'delete']);
 });
