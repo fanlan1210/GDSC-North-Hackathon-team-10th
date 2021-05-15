665791b5490858d08ed950e060c29991dd87529e
linwebs <thomas881114@gmail.com>
2021-05-15 23:53:16 +0800

    order create

--- backend/app/Models/OrderMeal.php
+++ backend/app/Models/OrderMeal.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class OrderMeal extends Model
 {
     use HasFactory;
+
+	protected $table = 'order_meals';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'order_id',
+		'meal_id',
+		'quantity',
+		'note'
+	];
+
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+
+	];
 }
