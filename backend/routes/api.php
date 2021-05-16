2e2f0920f190ca64f8d6460abaaf54648639a008
Ting-Jun Wang <levi900227@gmail.com>
2021-05-16 13:36:24 +0800

    feat: Add Cors middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,79 +22,80 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware(['cors'])->group(function(){
+    Route::prefix('/user')->group(function () {
+        Route::post('/login', [UserController::class, 'login']);
+        Route::post('/', [UserController::class, 'register']);
+    });
 
-Route::prefix('/user')->group(function () {
-	Route::post('/login', [UserController::class, 'login']);
-	Route::post('/', [UserController::class, 'register']);
-});
+    Route::prefix('/place_area')->group(function () {
+        Route::get('/', [PlaceAreaController::class, 'index']);
+        Route::get('/{id}', [PlaceAreaController::class, 'show']);
+        Route::get('/{id}/builds', [PlaceAreaController::class, 'getBuilds']);
+    });
 
-Route::prefix('/place_area')->group(function () {
-	Route::get('/', [PlaceAreaController::class, 'index']);
-	Route::get('/{id}', [PlaceAreaController::class, 'show']);
-	Route::get('/{id}/builds', [PlaceAreaController::class, 'getBuilds']);
-});
+    Route::prefix('/place_build')->group(function () {
+        Route::get('/{id}', [PlaceBuildController::class, 'show']);
+        Route::get('/{id}/rooms', [PlaceBuildController::class, 'getRooms']);
+    });
 
-Route::prefix('/place_build')->group(function () {
-	Route::get('/{id}', [PlaceBuildController::class, 'show']);
-	Route::get('/{id}/rooms', [PlaceBuildController::class, 'getRooms']);
-});
+    Route::prefix('/place_room')->group(function () {
+        Route::get('/{id}', [PlaceRoomController::class, 'show']);
+    });
 
-Route::prefix('/place_room')->group(function () {
-	Route::get('/{id}', [PlaceRoomController::class, 'show']);
-});
+    Route::prefix('/shop')->group(function () {
+        Route::get('/', [ShopController::class, 'index']);
+        Route::get('/{id}', [ShopController::class, 'show']);
+    });
 
-Route::prefix('/shop')->group(function () {
-	Route::get('/', [ShopController::class, 'index']);
-	Route::get('/{id}', [ShopController::class, 'show']);
-});
+    Route::prefix('/meal')->group(function () {
+        Route::get('/{id}', [MealController::class, 'show']);
+    });
 
-Route::prefix('/meal')->group(function () {
-	Route::get('/{id}', [MealController::class, 'show']);
-});
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('/user')->group(function () {
+            Route::get('/{id}', [UserController::class, 'show']);
+            Route::delete('/{id}', [UserController::class, 'delete']);
+            Route::get('/', [UserController::class, 'index']);
+        });
 
-Route::middleware('auth:sanctum')->group(function () {
-	Route::prefix('/user')->group(function () {
-		Route::get('/{id}', [UserController::class, 'show']);
-		Route::delete('/{id}', [UserController::class, 'delete']);
-		Route::get('/', [UserController::class, 'index']);
-	});
+        Route::prefix('/place_area')->group(function () {
+            Route::post('/', [PlaceAreaController::class, 'store']);
+            Route::post('/{id}/build', [PlaceBuildController::class, 'store']);
+        });
 
-	Route::prefix('/place_area')->group(function () {
-		Route::post('/', [PlaceAreaController::class, 'store']);
-		Route::post('/{id}/build', [PlaceBuildController::class, 'store']);
-	});
+        Route::prefix('/place_build')->group(function () {
+            Route::post('/{id}/room', [PlaceRoomController::class, 'store']);
+        });
 
-	Route::prefix('/place_build')->group(function () {
-		Route::post('/{id}/room', [PlaceRoomController::class, 'store']);
-	});
+        Route::prefix('/shop')->group(function () {
+            Route::post('/', [ShopController::class, 'store']);
+            Route::get('/{id}/orders', [OrderController::class, 'shop_index']);
+        });
 
-	Route::prefix('/shop')->group(function () {
-		Route::post('/', [ShopController::class, 'store']);
-		Route::get('/{id}/orders', [OrderController::class, 'shop_index']);
-	});
+        Route::prefix('/user_place')->group(function () {
+            Route::post('/', [UserPlaceController::class, 'store']);
+            Route::get('/{id}', [UserPlaceController::class, 'show']);
+            Route::get('/', [UserPlaceController::class, 'index']);
+            Route::delete('/{id}', [UserPlaceController::class, 'index']);
+        });
 
-    Route::prefix('/user_place')->group(function () {
-		Route::post('/', [UserPlaceController::class, 'store']);
-		Route::get('/{id}', [UserPlaceController::class, 'show']);
-		Route::get('/', [UserPlaceController::class, 'index']);
-		Route::delete('/{id}', [UserPlaceController::class, 'index']);
-	});
+        Route::prefix('/car')->group(function () {
+            Route::post('/', [CarController::class, 'append']);
+            Route::get('/', [CarController::class, 'index']);
+        });
 
-    Route::prefix('/car')->group(function () {
-		Route::post('/', [CarController::class, 'append']);
-        Route::get('/', [CarController::class, 'index']);
-	});
-
-	Route::prefix('/order')->group(function () {
-		Route::get('/', [OrderController::class, 'index']);
-		Route::post('/', [OrderController::class, 'store']);
-        Route::get('/{id}', [OrderController::class, 'show']);
-		Route::delete('/{id}', [OrderController::class, 'cancel']);
-		Route::put('/{id}/accept', [OrderController::class, 'accept']);
-		Route::put('/{id}/cook', [OrderController::class, 'cook']);
-		Route::put('/{id}/wait', [OrderController::class, 'wait']);
-		Route::put('/{id}/deliver', [OrderController::class, 'deliver']);
-		Route::put('/{id}/arrive', [OrderController::class, 'arrive']);
-		Route::put('/{id}/finish', [OrderController::class, 'finish']);
-	});
+        Route::prefix('/order')->group(function () {
+            Route::get('/', [OrderController::class, 'index']);
+            Route::post('/', [OrderController::class, 'store']);
+            Route::get('/{id}', [OrderController::class, 'show']);
+            Route::delete('/{id}', [OrderController::class, 'cancel']);
+            Route::put('/{id}/accept', [OrderController::class, 'accept']);
+            Route::put('/{id}/cook', [OrderController::class, 'cook']);
+            Route::put('/{id}/wait', [OrderController::class, 'wait']);
+            Route::put('/{id}/deliver', [OrderController::class, 'deliver']);
+            Route::put('/{id}/arrive', [OrderController::class, 'arrive']);
+            Route::put('/{id}/finish', [OrderController::class, 'finish']);
+        });
+    });
 });
